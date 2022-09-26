c13fbdef048f895ea8020c912ac0dd0e2d6c39da
Oklukeok <lucasabermudez@gmail.com>
2022-09-26 14:06:42 -0600

    Add form to html and an event in JS

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-/* 1. create an empty array to fill it with objects
+/* 1. create an empty array to fill it with objects ********************DONE
 
-2. the add button pushes an object into the array
+ 2. the add button ******************DONE
+ pushes an object into the array ****** NOT YET
 
-3. we need to display all added objects
+3. we need to display all added objects *************ALMOST
 
 4. every object has a remove button too
 
@@ -10,4 +11,34 @@
 
 6. 
 
+*/
+
+const bookApp = document.querySelector('#booksInput');
+
+const booksArray = [
+
+];
+
+document.getElementById('buttonInput').addEventListener('click', () => {
+    const seeMoreText = document.createElement('p');
+    seeMoreText.innerText = 'See More';
+    bookApp.appendChild(seeMoreText);
+
+
+});
+
+
+/*
+let title; 
+let authorINput; 
+
+
+book {
+    author = Shaksperae
+}
+
+function NewBook (title, author) {
+    new.title = input.title;
+    new.author= input.author;
+}
 */
