653d8d36f4ff4551ba5f4934b0ee1ac8b5eb8127
Matyas Gombos <gombosmm@gmail.com>
2022-09-26 14:02:48 -0500

    Add js pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 1. create an empty array to fill it with objects
+
+2. the add button pushes an object into the array
+
+3. we need to display all added objects
+
+4. every object has a remove button too
+
+5. remove  buttons have a function/event to remove the same object
+
+6. 
+
+*/
