582735168754a4343d495c1265fe6946627b8c80
Oklukeok <lucasabermudez@gmail.com>
2022-09-27 17:00:07 -0600

    Updated JS file

--- script.js
+++ script.js
@@ -13,87 +13,233 @@
 
 */
 
+
 const bookApp = document.querySelector('#booksInput');
 
-const booksArray = [
+//const booksArray = [
   
-];
+//];
 
-/*window.addEventListener('load', () => {
-  const arr = JSON.parse(window.localStorage.getItem('bookarr'));
-  const booksArray = document.querySelector('#booksInput');
-  /*arr.forEach((item) => {
-    booksArray.innerHTML += `<div class="bookDiv">
-    <h2 class="title-author">"${item.title}" by ${item.author}</h2>
-    <button id=${arr.indexOf(item)} onclick="deleting(this.id)" class="buttonDelete">Remove</button>
-    </div>`;
-  });
-});
-*/
-let i =0;
+let i;
+
+
 
 let titleInput;
+let authorInput;
+
+var bookDiv;
+
+if (localStorage.getItem('books') === null) {
+
+} else {
+  
+  const booksArray = JSON.parse(localStorage.getItem('books'));
+  
+  localStorage.setItem('books', JSON.stringify(booksArray));
+  let i = 0;
+  booksArray.forEach((item) => {
+  
+ 
+
+  bookDiv = document.createElement('div');
+  bookDiv.classList.add('NewBook');
+  bookDiv.setAttribute("id",'NewBook'+i);
+  bookApp.appendChild(bookDiv);
+  
+ const bookTitle = document.createElement('h2');
+ bookTitle.innerText = item.title;
+ bookDiv.appendChild(bookTitle);
+
+ const bookAuthor = document.createElement('h2');
+ bookAuthor.innerText = item.author;
+ bookDiv.appendChild(bookAuthor);
+
+ const buttonDelete = document.createElement('button');
+ buttonDelete.innerText = 'Remove';
+  buttonDelete.classList.add('buttonDelete');
+  buttonDelete.setAttribute("id","button"+i);
+  bookDiv.appendChild(buttonDelete);
+
+  document.getElementById("button"+i).addEventListener('click', () => {
+    
+    booksArray[i].setAttribute('id','delete');
+   // bookDiv=("id",'delete');
+    booksArray.splice(booksArray[i], 1);
+     
+     //bookDiv.setAttribute('id','delete');
+     console.log(booksArray)
+   });
+   i++;
+   
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* THE INPUTS*/
 document.getElementById('buttonInput').addEventListener('click', () => {
 
+
+
+
+
+
+
+
+
+
+
   titleInput = document.querySelector('#titleInput');
-  titleInput2 = titleInput.value
+  let titleInput2 = titleInput.value
 
   authorInput = document.querySelector('#authorInput');
-  authorInput2 = authorInput.value
+  let authorInput2 = authorInput.value;
     
   let newBook = {
-    id: i,
+   // id: i,
     title: titleInput2,
     author: authorInput2,
   }
   
-  booksArray.push(newBook);
-
- localStorage.setItem('newBook', JSON.stringify(newBook));
-
-  console.log(booksArray[i]);
+  //booksArray.push(newBook);
+  
 
 /* THE DOM*/
 
-  const bookDiv = document.createElement('div');
-  bookDiv.classList.add('NewBook'+i);
-  bookDiv.setAttribute("id",'NewBook'+i);
+  //const bookDiv = document.createElement('div');
+/*  bookDiv.classList.add('NewBook');
+  bookDiv.setAttribute("id",'NewBook');
   bookApp.appendChild(bookDiv);
     
-  const bookTitle = document.createElement('p');
+  const bookTitle = document.createElement('h2');
   bookTitle.innerText = newBook.title;
   bookDiv.appendChild(bookTitle);
 
-  const bookAuthor = document.createElement('p');
+  const bookAuthor = document.createElement('h2');
   bookAuthor.innerText = newBook.author;
   bookDiv.appendChild(bookAuthor);
 
   const buttonDelete = document.createElement('button');
   buttonDelete.innerText = 'Remove';
   buttonDelete.classList.add('buttonDelete');
-  buttonDelete.setAttribute("id","button"+i);
+  buttonDelete.setAttribute("id","button");
   bookDiv.appendChild(buttonDelete);
-
-  const divider = document.createElement('div');
-  divider.classList.add('divider');
-  bookDiv.appendChild(divider);
     
-  document.getElementById("button"+i).addEventListener('click', () => {
-    /*const booksTitle = document.createElement('h2');
-    booksTitle.innerText = "mandarina";
-    bookDiv.appendChild(booksTitle);
+  document.getElementById("button").addEventListener('click', () => {
+  
     
-    booksArray.splice(booksArray[i], 1);
-    */
+   // booksArray.splice(booksArray[i], 1);
     
     bookDiv.setAttribute('id','delete');
     console.log(booksArray)
   });
+  
+  */
+/*
+  if (localStorage.getItem('books') === null) {
+    const booksArray = [];
+    booksArray.push(newBook);
+    localStorage.setItem('books', JSON.stringify(booksArray));
+
+    const bookDiv = document.createElement('div');
+  bookDiv.classList.add('NewBook');
+  bookDiv.setAttribute("id",'NewBook');
+  bookApp.appendChild(bookDiv);
     
-  i++;
+  const bookTitle = document.createElement('h2');
+  bookTitle.innerText = newBook.title;
+  bookDiv.appendChild(bookTitle);
+
+  const bookAuthor = document.createElement('h2');
+  bookAuthor.innerText = newBook.author;
+  bookDiv.appendChild(bookAuthor);
+
+  const buttonDelete = document.createElement('button');
+  buttonDelete.innerText = 'Remove';
+  buttonDelete.classList.add('buttonDelete');
+  buttonDelete.setAttribute("id","button");
+  bookDiv.appendChild(buttonDelete);
+    
+  document.getElementById("button").addEventListener('click', () => {
+  
+    
+   // booksArray.splice(booksArray[i], 1);
+    
+    bookDiv.setAttribute('id','delete');
+    console.log(booksArray)
+  });
+
+  } else {
+    
+
+    const booksArray = JSON.parse(localStorage.getItem('books'));
+    booksArray.push(newBook);
+    localStorage.setItem('books', JSON.stringify(booksArray));
+  
+    
+    
+  
+    bookDiv = document.createElement('div');
+    bookDiv.classList.add('NewBook');
+    bookDiv.setAttribute("id",'NewBook');
+    bookApp.appendChild(bookDiv);
+    
+   const bookTitle = document.createElement('h2');
+   bookTitle.innerText = newBook.title;
+   bookDiv.appendChild(bookTitle);
+  
+   const bookAuthor = document.createElement('h2');
+   bookAuthor.innerText = newBook.author;
+   bookDiv.appendChild(bookAuthor);
+  
+    const buttonDelete = document.createElement('button');
+    buttonDelete.innerText = 'Remove';
+    buttonDelete.classList.add('buttonDelete');
+    buttonDelete.setAttribute("id","button");
+    bookDiv.appendChild(buttonDelete);
+
+
+   
+
+
+
+
+
+
+  }
+
+
+
+  
+
+
+
+*/
+
+
+
+
+
+
+
+  //i++;
+  
 });
 
 
